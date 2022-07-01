e8c0854702ccd47293c49b0a4557766064a8fe17
Batın Berkin Topaloğlu <topaloglubatinberkin@gmail.com>
2022-07-01 12:05:57 +0300

    Create generic_lists.dart

---
+++ flutter_giris/lib/generic_lists.dart
@@ -0,0 +1,32 @@
+/*import 'package:degiskenler/class.dart';
+
+void main() {
+  List<String> personeller = new List<String>(4);
+  personeller[0] = "Berkin";
+  personeller[1] = "Veli";
+  personeller[2] = "Hasret";
+  personeller[3] = "Selin";
+
+  print(personeller);
+
+  List departmanlar = ["Satın Alma", "İnsan Kaynakları"];
+  print(departmanlar);
+
+  List calisanlarListesi = [];
+  var personel1 = calisanlar("Batin", "Berkin", 22);
+  var personel2 = calisanlar("Ali", "Veli", 26);
+}
+
+class calisanlar {
+  String ad;
+  String soyad;
+  int yas;
+
+  calisanlar(String ad, String soyad, int yas) {
+    this.ad = ad;
+    this.soyad = soyad;
+    this.yas = yas;
+  }
+}
+
+*/
