ddb1f8502317a33ff778af0fc9a9e0570f901e95
BBerkinT <topaloglubatinberkin@gmail.com>
2022-11-03 13:11:42 +0300

    row_column_stack

---
+++ tasarim/row_column_stack/android/app/src/main/kotlin/com/example/row_column_stack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.row_column_stack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tasarim/row_column_stack/lib/column.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Text("Column"),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
