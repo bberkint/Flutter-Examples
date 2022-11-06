d6f3d8ca656eca1260341e238f91e298aa6be3cb
BBerkinT <topaloglubatinberkin@gmail.com>
2022-11-07 01:02:36 +0300

    oop_odev5

---
+++ nesne_tabanli_programlama/object_oriented_programming/lib/Odev5.dart
@@ -0,0 +1,6 @@
+class Odev5 {
+  int icAciToplama(int kenarSayisi) {
+    int toplam = (kenarSayisi - 2) * 180;
+    return toplam;
+  }
+}
---
+++ nesne_tabanli_programlama/object_oriented_programming/lib/Odev5Main.dart
@@ -0,0 +1,8 @@
+import 'package:object_oriented_programming/Odev5.dart';
+
+void main(List<String> args) {
+  var o5 = Odev5();
+
+  int sonuc = o5.icAciToplama(5);
+  print(sonuc);
+}
