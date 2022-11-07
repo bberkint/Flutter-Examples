8d04f8461711dcaad3788ec9c2d43d7bf4c364ed
BBerkinT <topaloglubatinberkin@gmail.com>
2022-11-07 13:59:29 +0300

    hizalama_islemleri

---
+++ hizalama_islemleri/hizalama_islemleri/android/app/src/main/kotlin/com/example/hizalama_islemleri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hizalama_islemleri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hizalama_islemleri/hizalama_islemleri/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(
+        mainAxisSize: MainAxisSize.min,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween, //Bulunduğu tasarım alanına göre ana eksende hizalama işlemi yapıyor
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            color: Colors.red,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.yellow,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
