e6ef3142905199740df2494cc0ec4f525ba78865
BBerkinT <topaloglubatinberkin@gmail.com>
2022-12-28 16:28:16 +0300

    image asset

---
+++ flutter_widgets/widgets/lib/imageLocal.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String resimAdi = "lebron.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("resimler/$resimAdi"),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  resimAdi = "kobe.jpeg";
+                });
+              },
+              child: Text("Kobe"),
+            ),
+            ElevatedButton(
+              child: Text("Lebron"),
+              onPressed: () {
+                setState(() {
+                  resimAdi = "lebron.jpg";
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
