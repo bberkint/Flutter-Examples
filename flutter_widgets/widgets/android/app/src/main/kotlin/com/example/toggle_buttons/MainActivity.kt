b2f95800b62c78219441a89b07c1b21dcff8870f
BBerkinT <topaloglubatinberkin@gmail.com>
2022-12-28 15:52:10 +0300

    webview widget

---
+++ flutter_widgets/widgets/android/app/src/main/kotlin/com/example/toggle_buttons/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.toggle_buttons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_widgets/widgets/lib/webview.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: const WebView(
+        initialUrl: "https://flutter.dev/",
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+
+    );
+  }
+}
